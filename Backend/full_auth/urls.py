b8dfae93c616b741b7b970c3c046ab8de1e60df0
shaurya690-ux <thakurshaurya690@gmail.com>
2025-07-20 20:52:52 +1000

    Add Backend/full_auth/urls.py 1753008771460

---
+++ Backend/full_auth/urls.py
@@ -0,0 +1,19 @@
+
+
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from inventory.views import ChemicalViewSet
+from users.views import LoginView
+
+router = DefaultRouter()
+router.register(r'chemicals', ChemicalViewSet)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('api/', include(router.urls)),
+    path("api/", include("djoser.urls")),
+    path("api/", include("djoser.urls.jwt")),
+    path("api/chatbot/", include("chatbot.urls")),
+    path("login/", LoginView.as_view(), name="login"),
+]
